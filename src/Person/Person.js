81e1968ddf6f75032238582d8afb33a3b5247907
Sean Z <contact@seanzeng.com>
2019-07-19 17:30:02 +1200

    css module implementation introduction

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.module.css';
+
+
+/**
+ * Component using css modules.
+ * It targets a component specifically by generating a unique class names
+ * Thus preventing similar classNames from inheriting each others undesired styles.
+ */
 
 const Person = (props) => {
 
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <span>I'm {props.name} and I am {props.age} years old.</span>
             <br/>
             <input type="text" onChange={props.change} value={props.name}/>
-            <button className="delete" onClick={props.delete}>X</button>
-            <div className="id">ID/Key: {props.id}</div>
+            <button className={classes.delete} onClick={props.delete}>X</button>
+            <div className={classes.id}>ID/Key: {props.id}</div>
         </div>
     )
 };
